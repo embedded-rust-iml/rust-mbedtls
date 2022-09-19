ff47b1ecd8fc6b1eade28ae949df82e5ee2a0dbc
Tobias Naumann <tobias.naumann@iml.fraunhofer.de>
2022-09-19 16:57:11 +0200

    Only include available headers for bindgen generation
    For bare-metal environments, some of the headers which were added to the
    bindgen process could be missing:
    - time.h is removed by depending on the "time" feature
    - threading.h is removed in the same way but this requires a new
      "threading" feature for mbedtls
    - a "no_std" feature for mbedtls-sys-auto allows to not include stdio.h,
      stdlib.h and time.h in platform.h anymore by defining
      MBEDTLS_PLATFORM_NO_STD_FUNCTIONS

--- mbedtls-sys/build/headers.rs
+++ mbedtls-sys/build/headers.rs
@@ -28,7 +28,7 @@ use crate::features::FEATURES;
 #[cfg_attr(rustfmt, rustfmt_skip)]
 pub const ORDERED: &'static [(Option<&'static str>, &'static str)] = &[
     (None,                 "config_psa.h"),
-    (None,                 "platform_time.h"),
+    (Some("time"),         "platform_time.h"),
     (None,                 "platform_util.h"),
     (None,                 "bignum.h"),
     (None,                 "md.h"),
